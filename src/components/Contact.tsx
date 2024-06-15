84f2b232f520c60b96299d6e9a6e2ec2636afd2c
AltinGruda <altingruda@gmail.com>
2024-06-15 15:40:13 +0200

    z index bug

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,7 @@ export function Contact() {
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex flex-col md:flex-row md:space-x-6 md:space-y-0 bg-indigo-500 w-full max-w-4xl p-8 sm:p-12 rounded-xl shadow-lg text-white overflow-hidden">
-        <div className="flex flex-col space-y-8 justify-between">
+        <div className="flex flex-col space-y-8 justify-between z-10">
           <div>
             <h1 className="font-bold text-4xl tracking-wide">Contact Us</h1>
             <p className="pt-2 text-indigo-100 text-sm">
